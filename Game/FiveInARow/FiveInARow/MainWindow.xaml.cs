dc4895ec57838203df9071820cad09f10c24a80c
bigbulu <wangjifan@163.com>
2017-01-17 09:07:20 +0800

    add board to UI

--- Game/FiveInARow/FiveInARow/MainWindow.xaml.cs
+++ Game/FiveInARow/FiveInARow/MainWindow.xaml.cs
@@ -23,6 +23,54 @@ namespace FiveInARow
         public MainWindow()
         {
             InitializeComponent();
+
+            PaintGridLine();
+            PaintDots();
+        }
+
+        private void PaintGridLine()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                var line = new Line();
+                line.Stroke = Brushes.Black;
+                line.StrokeThickness = 1;
+                line.X1 = 50;
+                line.X2 = 610;
+                line.Y1 = 50 + i * 40;
+                line.Y2 = line.Y1;
+
+                _board.Children.Add(line);
+
+                line = new Line();
+                line.Stroke = Brushes.Black;
+                line.StrokeThickness = 1;
+                line.X1 = 50 + i * 40;
+                line.X2 = line.X1;
+                line.Y1 = 50;
+                line.Y2 = 610;
+                _board.Children.Add(line);
+            }
+        }
+
+        private void PaintDots()
+        {
+            PaintDot(4, 4);
+            PaintDot(8, 8);
+            PaintDot(12, 4);
+            PaintDot(12, 12);
+            PaintDot(4, 12);
+        }
+
+        private void PaintDot(int i, int j)
+        {
+            var dot = new Ellipse();
+            dot.Width = 6;
+            dot.Height = 6;
+            dot.Fill = Brushes.Black;
+            Canvas.SetLeft(dot, i * 40 + 10 - 3);
+            Canvas.SetTop(dot, j * 40 + 10 - 3);
+            _board.Children.Add(dot);
         }
     }
 }
